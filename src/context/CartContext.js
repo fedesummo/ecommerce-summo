1c99f501848f4983a330926176c2a44d4409df02
fedesummo <federicosummo@outlook.com>
2022-03-29 19:44:21 -0300

    clase 10

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -13,36 +13,33 @@ export const CartContextProvider = ({children}) => {
         [cart]
     )
 
+    // Add an item to cart.
     const addItem = (item, quantity) => {
-        // Setting a copy of the cart to modify the values.
-        const cartCopy = cart
         // Filtering the cart to know if the product already exist.
-        const filter = cartCopy.filter( element => element.item.id == item.id )[0]
+        const filter = cart.filter( element => element.item.id == item.id )[0]
         if ( filter ) {
             // If there's a coincidence, add the quantity...
             filter.quantity += quantity
             // ... and replace the original cart.
-            setCart(cartCopy)
-            console.log("Cart: " , cart)
+            setCart( [...cart] )
         } else {
             // If there´s not a coincidence, add the new product to the cart.
             setCart( [...cart, {item: item, quantity: quantity}] )
         }
     }
 
+    // Remove an item from cart.
     const removeItem = id => {
-        // Setting a copy of the cart to modify the values.
-        const cartCopy = cart
         // Getting the index of the element.
         const index = cart.indexOf( cart.filter( element => element.item.id == id )[0] )
-        cartCopy.splice(index, 1)
-        setCart(cartCopy)
-        console.log(cart)
+        cart.splice(index, 1)
+        setCart([...cart])
     }
 
     // Clear cart.
     const clear = () => setCart([])
 
+    // Check if an item is in cart.
     const isInCart = id => cart.filter( element => element.item.id == id ).length > 0 ? true : false
 
     return <CartContext.Provider value={{cart, addItem, clear, isInCart, removeItem}}> {children} </CartContext.Provider>
